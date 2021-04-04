61cf910e028fb8bd0e1ba103039ea4578e1b05c5
Allani Ahmed <67459843+ahmedallani@users.noreply.github.com>
2021-04-05 00:00:28 +0100

    CRUD section page

--- server/routes/api/sections.js
+++ server/routes/api/sections.js
@@ -3,6 +3,8 @@ const { section } = require("../../controllers");
 
 router.post("/", section.create);
 router.get("/:id?", section.read);
+router.patch("/:id", section.update);
+router.delete("/:id", section.delete);
 
 
 module.exports = router;
